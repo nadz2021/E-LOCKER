8d0aec332ac619016e2672fe0d2df6844ccee8e5
aspienadela <54401524+aspienadela@users.noreply.github.com>
2020-04-06 20:57:06 -0700

    extract
    extract files transfer to the root

---
+++ index.php
@@ -0,0 +1,213 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>elock ph</title>
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/styles.css" rel="stylesheet">
+        <!--[if lt IE 9]>
+          <script src="js/html5shiv.js"></script>
+          <script src="js/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-sm-8 col-xs-12">
+                        <img id="logo" src="SLICED/logo.png"/>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <div id="header-button" class="pull-right">
+                            <button type="button" class="btn btn-primary">LOGIN</button>
+                            <button type="button" class="btn btn-primary">SIGN UP</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">HOME</a></li>
+                        <li><a href="#">ABOUT US</a></li>
+                        <li><a href="#">OUR SERVICES</a></li>              
+                        <li><a href="#">LOCATIONS</a></li>
+                        <li><a href="#">INVESTORS</a></li>
+                        <li><a href="#">GET LOCKERS</a></li>
+                        <li><a href="#">SUPPORT</a></li>
+                    </ul>            
+                </div><!--/.nav-collapse -->
+            </div><!--/.container -->
+        </nav>
+        <div id="main-banner">
+            <img id="home-banner" src="SLICED/home/homebanner.jpg" style="width:100%;"/>
+            <div class="container">
+                <div class="main_content">
+                <h2>We put lockers at your doorstep<br/>to give you a hassle free laundry service 24/7</h2>
+                <br/>
+                <button class="btn-lg btn btn-primary">LEARN MORE</button>
+                </div>
+            </div>
+        </div>
+        <div class="how_it_works">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5 col-sm-5">
+                        <h1>How it works</h1>
+                        <h2>Our Locker Service <br/>is Quick and Easy to Use.</h2>
+                    </div>
+                    <div class="col-md-7 col-sm-7">
+                        <img src="SLICED/home/howitworks.jpg" class="img-responsive img_center"/>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="no_locker">
+            <div class="container">
+                <h1>No locker in your building just yet?</h1>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="img-circle center-block locker_item">
+                            <center>
+                                <h3>USE OUR APP</h3>
+                                <p><small>Get your laundry done with just a press of <br>a button</small></p>
+                            </center>
+                            <a href="#"><div class="app_button">DOWNLOAD APP</div></a>
+                        </div>
+                        <div class="img-circle center-block locker_item">
+                            <center>
+                                <h3>ORDER ONLINE</h3>
+                                <p><small>Clean clothes <br>are just a click away</small></p>
+                            </center>
+                            <a href="#"><div class="app_button">REQUEST PICK-UP</div></a>
+                        </div>
+                        <div class="img-circle center-block locker_item">
+                            <center>
+                                <h3>CALL US</h3>
+                                <p><small>Give us a call.<br/>
+                                We will gladly collect<br/>
+                                and delivery<br/>
+                                your laundry</small></p>                                
+                            </center>
+                            <div class="app_button">032 232-1234</div>
+                        </div>
+                    </div>                    
+                </div>
+                <img src="SLICED/home/cellphone.jpg" class="img-responsive cell_img"/>
+            </div>
+        </div>
+        <div class="testimonial">
+            <div class="container">
+                <div  class="row">
+                    <div class="col-md-8 pull-right">
+                        <div class="testimonial_content pull-right">
+                            <div class="quote"><img src="SLICED/home/quote.png" style="width:60%;"/></div>
+                            <h3>Fantastic quality and customer support.</h3><br/>
+                            <h4 class="text-center">-Dina L.</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <div class="laundry_partners">
+            <div class="container">
+                <h1 class="text-center">Laundry shop partners</h1>
+                <img src="SLICED/home/laundryshops.png" class="img-responsive" />
+            </div>
+        </div>
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="footer_links">                        
+                            <div class="row">
+                                <div class="col-md-3 col-sm-3 col-xs-3">
+                                    <div class="link_lists">
+                                        <h6>INFO</h6>
+                                        <ul>
+                                            <li><a href="#">FAQ's</a></li>
+                                            <li><a href="#">In the News</a></li>
+                                            <li><a href="#">Franchise</a></li>
+                                            <li><a href="#">Request Locker</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 col-sm-3 col-xs-3">
+                                    <div class="link_lists">
+                                        <h6>ABOUT US</h6>
+                                        <ul>
+                                            <li><a href="#">Our Story</a></li>
+                                            <li><a href="#">Our Manifesto</a></li>
+                                            <li><a href="#">Our Quality</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 col-sm-3 col-xs-3">
+                                    <div class="link_lists">
+                                        <h6>LEGAL </h6>
+                                        <ul>
+                                            <li><a href="#">Terms</a></li>
+                                            <li><a href="#">Privacy Policy</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-md-3 col-sm-3 col-xs-3">
+                                    <div class="link_lists">
+                                        <h6>ACCOUNT</h6>
+                                        <ul>
+                                            <li><a href="#">Sign Up</a></li>
+                                            <li><a href="#">Log In</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <ul class="social_media_links pull-right">
+                            <li><a href="#"><img src="SLICED/footer_fb.png"/></a></li>
+                            <li><a href="#"><img src="SLICED/footer_tw.png"/></a></li>
+                            <li><a href="#"><img src="SLICED/footer_yt.png"/></a></li>
+                            <li><a href="#"><img src="SLICED/footer_gplus.png"/></a></li>
+                            <li><a href="#"><img src="SLICED/footer_inst.png"/></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script>           
+            (function() {
+                function textAtCenter(){
+                   if($(window).width()>480){
+                    $('.main_content').css('top',($('#home-banner').height()/2 - $('h2').height()) + 'px');
+                }
+                else {
+                    $('.main_content').css('top',0 + 'px');
+                }
+                }
+
+                $(window).on("resize", textAtCenter);
+                $(document).on("ready", textAtCenter);
+            })();
+        </script>
+    </body>
+
+</html>
